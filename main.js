643d58c698427575359333d9f9d8deb6c2a3faa6
Paul Power <power_604@msn.com>
2022-11-12 00:30:09 +0000

    Added JS for Mobile View
    Added JS to close mobile view menu after clicking and updating styling to sections background colours

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ window.onload = function digital_fn(){
         //console.log(toggle)
     });
 
+    // Close mobile view menu after click
+    document.querySelectorAll(".collapse").forEach(n => n.addEventListener ("click", () =>{
+      toggle.classList.remove("active");
+      collapse.classList.remove("active");
+    }))
+
     //masonry js
     let grid = document.querySelector("#site-main .merch-area .images-flex");
 
